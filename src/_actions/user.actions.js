690ae20c5d15edd52bffe01b5f833402fa03eda7
justbodia <havano.bohdan@gmail.com>
2019-02-27 13:44:41 +0200

    fix alignment in actions.js

--- src/_actions/user.actions.js
+++ src/_actions/user.actions.js
@@ -4,105 +4,106 @@ import { alertActions } from './';
 import { history } from '../_helpers';
 
 export const userActions = {
-    login,
-    logout,
-    register,
-    getAll,
-    newQuestion,
-    getAllQuestions
+  login,
+  logout,
+  register,
+  getAll,
+  newQuestion,
+  getAllQuestions
 };
 
 function login(username, password) {
-    return dispatch => {
-        dispatch(request({ username }));
+  return dispatch => {
+    dispatch(request({ username }));
 
-        userService.login(username, password)
-            .then(
-                user => {
-                    dispatch(success(user));
-                    history.push('/');
-                },
-                error => {
-                    dispatch(failure(error));
-                    dispatch(alertActions.error(error));
-                }
-            );
-    };
+    userService.login(username, password)
+      .then(
+        user => {
+          dispatch(success(user));
+          history.push('/');
+        },
+        error => {
+          dispatch(failure(error));
+          dispatch(alertActions.error(error));
+        }
+      );
+  };
 
-    function request(user) { return { type: userConstants.LOGIN_REQUEST, user } }
-    function success(user) { return { type: userConstants.LOGIN_SUCCESS, user } }
-    function failure(error) { return { type: userConstants.LOGIN_FAILURE, error } }
+  function request(user) { return { type: userConstants.LOGIN_REQUEST, user } }
+  function success(user) { return { type: userConstants.LOGIN_SUCCESS, user } }
+  function failure(error) { return { type: userConstants.LOGIN_FAILURE, error } }
 }
 
 function logout() {
-    userService.logout();
-    return { type: userConstants.LOGOUT };
+  userService.logout();
+  return { type: userConstants.LOGOUT };
 }
 
 function register(user) {
-    return dispatch => {
-        dispatch(request(user));
+  return dispatch => {
+    dispatch(request(user));
 
-        userService.register(user)
-            .then(
-                user => {
-                    dispatch(success());
-                    history.push('/login');
-                    dispatch(alertActions.success('Registration successful'));
-                },
-                error => {
-                    dispatch(failure(error));
-                    dispatch(alertActions.error(error));
-                }
-            );
-    };
+    userService.register(user)
+      .then(
+        user => {
+          dispatch(success());
+          history.push('/login');
+          dispatch(alertActions.success('Registration successful'));
+        },
+        error => {
+          dispatch(failure(error));
+          dispatch(alertActions.error(error));
+        }
+      );
+  };
 
-    function request(user) { return { type: userConstants.REGISTER_REQUEST, user } }
-    function success(user) { return { type: userConstants.REGISTER_SUCCESS, user } }
-    function failure(error) { return { type: userConstants.REGISTER_FAILURE, error } }
+  function request(user) { return { type: userConstants.REGISTER_REQUEST, user } }
+  function success(user) { return { type: userConstants.REGISTER_SUCCESS, user } }
+  function failure(error) { return { type: userConstants.REGISTER_FAILURE, error } }
 }
+
 function newQuestion(question) {
-    userService.newQuestion(question)
-        .then(
-            question => {
-                success();
-                alertActions.success('new question');
-            },
-            error => {
-                dispatch(failure(error));
-                dispatch(alertActions.error(error));
-            }
-        );
+  userService.newQuestion(question)
+    .then(
+      question => {
+        success();
+        alertActions.success('new question');
+      },
+      error => {
+        dispatch(failure(error));
+        dispatch(alertActions.error(error));
+      }
+    );
 }
 
 function getAll() {
-    return dispatch => {
-        dispatch(request());
+  return dispatch => {
+    dispatch(request());
 
-        userService.getAll()
-            .then(
-                users => dispatch(success(users)),
-                error => dispatch(failure(error))
-            );
-    };
+    userService.getAll()
+      .then(
+        users => dispatch(success(users)),
+        error => dispatch(failure(error))
+      );
+  };
 
-    function request() { return { type: userConstants.GETALL_REQUEST } }
-    function success(users) { return { type: userConstants.GETALL_SUCCESS, users } }
-    function failure(error) { return { type: userConstants.GETALL_FAILURE, error } }
+  function request() { return { type: userConstants.GETALL_REQUEST } }
+  function success(users) { return { type: userConstants.GETALL_SUCCESS, users } }
+  function failure(error) { return { type: userConstants.GETALL_FAILURE, error } }
 }
 
 function getAllQuestions() {
-    return dispatch => {
-        dispatch(request());
+  return dispatch => {
+    dispatch(request());
 
-        userService.getAllQuestions()
-            .then(
-                questions => dispatch(success(questions)),
-                error => dispatch(failure(error))
-            );
-    };
+    userService.getAllQuestions()
+      .then(
+        questions => dispatch(success(questions)),
+        error => dispatch(failure(error))
+      );
+  };
 
-    function request() { return { type: userConstants.GETALLQUESTIONS_REQUEST } }
-    function success(questions) { return { type: userConstants.GETALLQUESTIONS_SUCCESS, questions } }
-    function failure(error) { return { type: userConstants.GETALLQUESTIONS_FAILURE, error } }
+  function request() { return { type: userConstants.GETALLQUESTIONS_REQUEST } }
+  function success(questions) { return { type: userConstants.GETALLQUESTIONS_SUCCESS, questions } }
+  function failure(error) { return { type: userConstants.GETALLQUESTIONS_FAILURE, error } }
 }
